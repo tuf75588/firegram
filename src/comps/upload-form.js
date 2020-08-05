8452ce12e51131ae64d76f54c0693ebc5824459c
Andrew Davis <atd285@gmail.com>
2020-08-04 20:50:43 -0400

    catching errors for non image files

--- src/comps/upload-form.js
+++ src/comps/upload-form.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function UploadForm() {
   function handleFileUpload(event) {
+    const allowedTypes = ['image/png', 'image/jpeg'];
     // grab the first file from our upload (only allowing one at a time)
     const input = event.currentTarget.files[0];
     console.log(input);
+    if (!input || !allowedTypes.includes(input.type)) {
+      // if they hit cancel
+      setFile(null);
+      setError('Please select an image file (png or jpg)');
+    }
+    setFile(input);
   }
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
       <input type="file" onChange={handleFileUpload} />
+      <div className="output">
+        {error && <p style={{ color: 'red', fontWeight: 'bold' }}>{error}</p>}
+      </div>
     </form>
   );
 }
