b6b6d968e7fa02c35dcc31a0aeb585f972f866ed
Andrew Davis <atd285@gmail.com>
2020-08-07 22:16:39 -0400

    finish useFirestore hook

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,3 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { firestore as projectFirestore } from '../firebase/config';
-function useFirestore() {}
+function useFirestore(collection) {
+  useEffect(() => {
+    // reach into a collection, listen for changes
+    const unsubscribe = projectFirestore
+      .collection(collection)
+      .orderBy('createdAt', 'desc')
+      .onSnapshot((snap) => {
+        // callback fires once initially, and on collection changes
+        let documents = [];
+        snap.forEach((doc) => {
+          documents.push({ ...doc.data(), id: doc.id() });
+        });
+        setDocs(documents);
+      });
+    return () => unsubscribe();
+  }, [collection]);
+
+  const [docs, setDocs] = useState([]);
+  return { docs };
+}
+
+export default useFirestore;
